b0345493a31aa2f40e3b72b4b169f81618155b6e
bogus <boguslaw.fraszko@gmail.com>
2019-10-03 00:24:33 +0200

    '4'

--- scripts/create-pg-source.sh
+++ scripts/create-pg-source.sh
@@ -3,16 +3,23 @@ curl -i -X POST \
     -H  "Content-Type:application/json" \
     http://connect-db:8083/connectors/ -d '
     {
-	  "name": "inventory-connector",
+	  "name": "td-connector",
 	  "config": {
 		  "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
 		  "tasks.max": "1",
-		  "database.hostname": "0.0.0.0",
+		  "database.hostname": "postgres",
 		  "database.port": "5432",
 		  "database.user": "postgres",
 		  "database.password": "postgres",
 		  "database.dbname" : "postgres",
 		  "database.server.name": "dbserver1",
-		  "schema.whitelist": "inventory"
+		  "schema.whitelist": "td",
+		  "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+		  "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+		  "transforms": "unwrap, flatten",
+		  "transforms.unwrap.type": "io.debezium.transforms.ExtractNewRecordState",
+		  "transforms.unwrap.operation.header":"true",
+		  "transforms.flatten.type": "org.apache.kafka.connect.transforms.Flatten$Value",
+		  "transforms.flatten.delimiter": "."
 		  }
 	 }'
